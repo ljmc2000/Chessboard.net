70c6ab780449552fcdf1d92c32f539c9eff3317d
Liam McCormick <ljmc2000@gmail.com>
2018-11-10 14:05:12 +0000

    added ability to sign up using HTTPAPI

--- httpapi/index.py
+++ httpapi/index.py
@@ -1,6 +1,7 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 from pymongo import MongoClient
 from bson.json_util import dumps
+import bcrypt
 
 app=Flask(__name__)
 db=MongoClient().ChessboardNet
@@ -9,3 +10,14 @@ db=MongoClient().ChessboardNet
 def hello_world():
 	returnme=dumps(db.test.find())
 	return jsonify(returnme)
+
+@app.route("/signup",methods=["POST"])
+def create_user():
+	try:
+		username=request.form.get("username")
+		password=request.form.get("password")
+		passhash=bcrypt.hashpw(password.encode(),bcrypt.gensalt())
+		db.users.insert({"username":username,"passhash":passhash})
+		return "success"
+	except:
+		return "failure"
