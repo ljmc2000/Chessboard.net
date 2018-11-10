fe816f6cd39b2d6de89a98b7a034fc07b0dce261
Liam McCormick <ljmc2000@gmail.com>
2018-11-10 16:27:29 +0000

    added sign in function to flask app

--- httpapi/README.md
+++ httpapi/README.md
@@ -1,3 +1,5 @@
 # The following dependencies are required of the HTTPAPI
+Python >= 3.6<br>
 Flask<br>
 Mongodb and pymongo<br>
+bcrypt<br>
--- httpapi/index.py
+++ httpapi/index.py
@@ -1,15 +1,16 @@
 from flask import Flask,jsonify,request
 from pymongo import MongoClient
 from bson.json_util import dumps
-import bcrypt
+import bcrypt,secrets
 
 app=Flask(__name__)
 db=MongoClient().ChessboardNet
 
+
 @app.route("/")
 def hello_world():
-	returnme=dumps(db.test.find())
-	return jsonify(returnme)
+	returnme=db.test.find_one()
+	return app.response_class(response=dumps(returnme),status=200,mimetype="application/json")
 
 @app.route("/signup",methods=["POST"])
 def create_user():
@@ -21,3 +22,18 @@ def create_user():
 		return "success"
 	except:
 		return "failure"
+
+@app.route("/signin",methods=["POST"])
+def get_login_token():
+	try:
+		username=request.form.get("username")
+		password=request.form.get("password")
+		userdetails=db.users.find_one({"username":username})
+		if bcrypt.checkpw(password.encode(),userdetails["passhash"]):
+			login_token=secrets.token_urlsafe(16)
+			db.user_tokens.insert({"_id":login_token,"user_id":userdetails["_id"]})
+			return jsonify({"status":"success","token":login_token})
+		else:
+			return jsonify({"status":"fail"})
+	except:
+		return jsonify({"status":"exception"})
