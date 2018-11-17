2380f776c6bd3bd0fdca90d28d5a6fd0098678d2
Liam McCormick <ljmc2000@gmail.com>
2018-11-17 19:22:09 +0000

    changed path in get-deps for chessboardnet server

--- chessboardnetServer/get_dependencies.sh
+++ chessboardnetServer/get_dependencies.sh
@@ -2,6 +2,6 @@ wget https://oss.sonatype.org/content/repositories/releases/org/mongodb/mongodb-
 wget https://oss.sonatype.org/content/repositories/releases/org/mongodb/mongodb-driver-core/3.8.1/mongodb-driver-core-3.8.1.jar
 wget https://oss.sonatype.org/content/repositories/releases/org/mongodb/bson/3.8.1/bson-3.8.1.jar
 
-javac -d . ../app/src/main/java/net/ddns/gingerpi/chessboardnetCommon/*.java
+javac -d . ../androidClient/app/src/main/java/net/ddns/gingerpi/chessboardnetCommon/*.java
 jar cf chessboardnetCommon.jar net
 rm -r net
