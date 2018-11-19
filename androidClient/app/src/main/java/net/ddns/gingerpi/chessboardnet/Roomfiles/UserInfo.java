c7b64543c9f02a9f8a693ade03938c9870cb557d
Liam McCormick <ljmc2000@gmail.com>
2018-11-19 23:09:31 +0000

    removed unused imports

--- androidClient/app/src/main/java/net/ddns/gingerpi/chessboardnet/Roomfiles/UserInfo.java
+++ androidClient/app/src/main/java/net/ddns/gingerpi/chessboardnet/Roomfiles/UserInfo.java
@@ -3,15 +3,6 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
-import com.android.volley.Response;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.Request;
-
-import net.ddns.gingerpi.chessboardnet.R;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 @Entity
 public class UserInfo {
     @PrimaryKey
--- androidClient/app/src/main/java/net/ddns/gingerpi/chessboardnet/Roomfiles/UserInfoDao.java
+++ androidClient/app/src/main/java/net/ddns/gingerpi/chessboardnet/Roomfiles/UserInfoDao.java
@@ -1,10 +1,7 @@
 package net.ddns.gingerpi.chessboardnet.Roomfiles;
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
-import android.arch.persistence.room.Update;
-import java.util.List;
 
 @Dao
 public interface UserInfoDao {
